018647400807557c24ef4b50139962a13f9f3658
msnow01 <msnow01@mail.uoguelph.ca>
2018-11-22 16:59:40 -0500

    recpathca bugs

--- inc/header.php
+++ inc/header.php
@@ -15,7 +15,7 @@
        <script src='https://www.google.com/recaptcha/api.js'></script>
        <script>
            function recaptchaCallback() {
-               $('#submit').removeAttr('disabled');
+               $('#sub').removeAttr('disabled');
            }
        </script>
 
@@ -24,16 +24,5 @@
 <?PHP 
 
 include "connection.php";
-
- function randomKey($length) {
-    $pool = array_merge(range(0,9), range('a', 'z'),range('A', 'Z'));
-
-    for($i=0; $i < $length; $i++) {
-        $key .= $pool[mt_rand(0, count($pool) - 1)];
-    }
-    return $key;
-}
-
-$randVar = randomKey(20);
 
 ?>

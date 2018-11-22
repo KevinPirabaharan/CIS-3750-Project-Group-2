5cd9a6a737cad1def4741c34a6b76220c3179263
EC2 Default User <ec2-user@ip-172-31-46-131.us-east-2.compute.internal>
2018-11-22 23:06:14 +0000

    Fixed databases

--- create_orangeDB.sql
+++ create_orangeDB.sql
@@ -1,8 +1,8 @@
 CREATE TABLE pinfo (
-    pinfoID    numeric(4), not null,
-    fname   varchar(15), not null,
+    pinfoID    numeric(4) not null,
+    fname   varchar(15) not null,
     lname   varchar(15),
-    email   varchar(30), not null,
+    email   varchar(30) not null,
     primary key(pinfoID)
 );
 
@@ -10,25 +10,25 @@ CREATE TABLE user (
     userID  numeric(4) not null,
     uName   varchar(30) not null,
     passwd  varchar(30) not null,
-    pinfoID    numeric(4), not null,
-    Role    varchar(5), not null,
+    pinfoID    numeric(4) not null,
+    Role    varchar(5) not null,
     primary key(userId),
     foreign key(pinfoID) references pinfo(pinfoID)
 );
 
 CREATE TABLE slides (
-    slideID numeric(4), not null,
-    bgColor varchar(16), not null,
-    grid    numeric(2), not null,
-    transition  numeric(2), not null,
+    slideID numeric(4) not null,
+    bgColor varchar(16) not null,
+    grid    numeric(2) not null,
+    transition  numeric(2) not null,
     primary key(slideID)
 );
 
 CREATE TABLE widgets (
-    widgetID    numeric(4), not null,
-    type    varchar(15), not null,
+    widgetID    numeric(4) not null,
+    type    varchar(15) not null,
     content varchar(256),
-    slideID, numeric(4), not null,
+    slideID numeric(4) not null,
     primary key(widgetID),
     foreign key(slideID) references slides(slideID)
 );

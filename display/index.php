d015ff13258b544268fc70ab11f36308f8479100
msnow01 <msnow01@mail.uoguelph.ca>
2018-11-22 17:17:43 -0500

    Update display/index.php

--- display/index.php
+++ display/index.php
@@ -160,7 +160,7 @@
         </tr>
 
         <tr>
-            <td colspan="2" style="align-content: center; text-align: center; vertical-align: 10%"><a href="../index.php"><img src="../images/logo.png" width="2%"> <br>Picturesque Designs</a> </td>
+            <td colspan="2" style="align-content: center; text-align: center; vertical-align: 10%"><a style="text-decoration:none" href="../index.php"><img src="../images/logo.png" width="2%"><br>Picturesque Designs</a></td>
         </tr>
     </table>
 

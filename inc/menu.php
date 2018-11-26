1cfaee460c413aae5cb801834e60a43a0f136dc0
msnow01 <msnow01@mail.uoguelph.ca>
2018-11-26 18:58:09 -0500

    Update menu.php

--- inc/menu.php
+++ inc/menu.php
@@ -49,6 +49,8 @@
     right: 25px;
     font-size: 36px;
     margin-left: 50px;
+    color: white;
+    text-decoration:none;
 }
 
 @media screen and (max-height: 450px) {
@@ -60,16 +62,15 @@
 
 <div id="mySidenav" class="sidenav">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-    <span style="margin-left:30px";>
-        <h5 class="text-white h4">Menu</h5>
+    <div style="margin-left:30px";>
+        <h5 class="text-white h4">Menu</h5><br>
         <p class="text-muted"><a href="<?php echo $dir; ?>index.php?login=1">Home</a></p>
         <p class="text-muted"><a href="<?php echo $dir; ?>create/index.php?login=1">Create</a></p>
+        <p class="text-muted"><a href="<?php echo $dir; ?>display/index.php?login=1">Preview</a></p>
         <p class="text-muted">Drafts</p>
         <p class="text-muted">Approvals</p>
-        <p class="text-muted"><a href="<?php echo $dir; ?>index.php?login=0">Logout</a></p>
-        <br>
-        <p class="text-muted"><a href="<?php echo $dir; ?>display/index.php?login=1">Preview</a></p>
-    </span>
+        <p class="text-muted"><a href="<?php echo $dir; ?>index.php?login=0">Logout</a></p>        
+    </div>
 </div>
 
     <nav class="navbar navbar-dark" style="background-color: #41A7C7">

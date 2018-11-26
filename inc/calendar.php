e9129e60986cd061775c069f48c243597818cab9
msnow01 <msnow01@mail.uoguelph.ca>
2018-11-26 12:54:38 -0500

    Update calendar.php

--- inc/calendar.php
+++ inc/calendar.php
@@ -18,8 +18,8 @@
             for ($i = 1; $i <= 7; $i++){
                 echo '<div class="calendar__day day">';
                 echo '<a href="../create/index.php?login=1&day=nov'.$i.'">'.$i.'</a>';
-                if ($i == 1){
-                    echo "<i class="material-icons">grade</i>"
+                if ($i == 3){
+                    echo "<i class='material-icons'>grade</i>"
                 }
                 echo '</div>';
                 

9e2d22f9436cb7f1e6c69ebd0844de424eb45249
msnow01 <msnow01@mail.uoguelph.ca>
2018-11-26 15:24:35 -0500

    Update menu.php

--- inc/menu.php
+++ inc/menu.php
@@ -17,7 +17,7 @@
             <p class="text-muted">Approvals</p>
             <p class="text-muted"><a href="<?php echo $dir; ?>index.php?login=0">Logout</a></p>
             <br>
-            <p class="text-muted"><a href="<?php echo $dir; ?>display/index.php?login=1">Display</p>
+            <p class="text-muted"><a href="<?php echo $dir; ?>display/index.php?login=1">Preview</p>
         </div>
     </div>
 </div>

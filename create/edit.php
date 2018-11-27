338b2826fba4ac23553c916d520137b43503d842
msnow01 <msnow01@mail.uoguelph.ca>
2018-11-27 11:53:15 -0500

    Update edit.php

--- create/edit.php
+++ create/edit.php
@@ -14,9 +14,9 @@ $day = $_GET['day'];
         <h1>Create New Slide</h1><br>
             <form method="get" action="">
                 <div class="row">
-                <div class="col-md-1">
+                <!--<div class="col-md-1">
                     <img src="../images/2x2.jpg" class="gridImg"><br>
-                    <input class="gridRadio" type="radio">
+                    <input class="gridRadio" checked type="radio">
                 </div>
                 <div class="col-md-1">
                     <img src="../images/3x3.jpg" class="gridImg"><br>
@@ -29,7 +29,16 @@ $day = $_GET['day'];
                 <div class="col-md-3">
                     <img src="../images/varied.jpg" class="gridImg"><br>
                     <input class="gridRadio" disabled type="radio">
-                </div>
+                </div>-->
+                <div class="col-md-4">
+                <p>Grid Layout: <select>
+                <option value="2x2"><img src="../images/2x2.jpg" class="gridImg"></option>
+                <option value="3x3"><img src="../images/3x3.jpg" class="gridImg"></option>
+                <option value="3x1"><img src="../images/3x1.jpg" class="gridImg"></option>
+                <option value="varied"><img src="../images/varied.jpg" class="gridImg"></option>
+                </select></p>
+                    </div>
+                  
                     
                 <div class="col-md-2">
                 <p>Transition: <select>
@@ -86,23 +95,6 @@ $day = $_GET['day'];
                 </tr>
                 </table>
                 <br>
-                <!--<div style="border:solid black 1px">
-                    <h2>Widgets</h2>
-                <div class="row">
-                    <div class="col-md-2" style="border:solid black 1px">
-                    <p>Text</p>
-                    </div>  
-                    <div class="col-md-2" style="border:solid black 1px">
-                    <p>Weather</p>
-                    </div>
-                    <div class="col-md-2" style="border:solid black 1px">
-                    <p>Time</p>
-                    </div>
-                    
-                </div>
-                
-                
-                </div>-->
                 <br>
                 <img src="../images/widgets.png">
                 <br><br>

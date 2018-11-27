1660828da97c0fc3afa0db0ccf4e531422da214d
msnow01 <msnow01@mail.uoguelph.ca>
2018-11-27 11:23:39 -0500

    Update stepTwo.php

--- create/stepTwo.php
+++ create/stepTwo.php
@@ -13,6 +13,27 @@ $day = $_GET['day'];
     <div class="textbox">
         <h1>Create New Slide</h1><br>
             <form method="get" action="">
+                <div class="row">
+                <div class="col-md-2">
+                    <img src="../images/2x2.jpg" class="gridImg"><br>
+                    <input class="gridRadio" type="radio">
+                </div>
+                <div class="col-md-2">
+                    <img src="../images/3x3.jpg" class="gridImg"><br>
+                    <input class="gridRadio" disabled type="radio">
+                </div>
+                <div class="col-md-2">
+                    <img src="../images/3x1.jpg" class="gridImg"><br>
+                    <input class="gridRadio" disabled type="radio">
+                </div>
+                <div class="col-md-2">
+                    <img src="../images/varied.jpg" class="gridImg"><br>
+                    <input class="gridRadio" disabled type="radio">
+                </div>
+                </div>
+                
+                
+                
                 <table border="1" cellpadding="50">
                 <tr>
                 <td>
@@ -52,6 +73,23 @@ $day = $_GET['day'];
                 </tr>
                 </table>
                 <br>
+                <div style="border:solid black 1px">
+                    <h2>Widgets</h2>
+                <div class="row">
+                    <div class="col-md-2" style="border:solid black 1px">
+                    <p>Text</p>
+                    </div>  
+                    <div class="col-md-2" style="border:solid black 1px">
+                    <p>Weather</p>
+                    </div>
+                    <div class="col-md-2" style="border:solid black 1px">
+                    <p>Time</p>
+                    </div>
+                    
+                </div>
+                
+                
+                </div>
                 <a href="../index.php?login=1&day=<?php echo $day; ?>">Finish&nbsp;<i class="material-icons">arrow_right_alt</i></a>
             </form>
         </div>
@@ -59,12 +97,12 @@ $day = $_GET['day'];
     
 <style>
 .gridImg {
-    width:150px;
-    height:150px;
+    width:50px;
+    height:50px;
     margin-right:1%;
 }
 
 .gridRadio {
-    margin-left:65px;
+    margin-left:25px;
 }
 </style>

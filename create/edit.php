19cbac270caf171634f0ce8ddf50ec15583abb00
msnow01 <msnow01@mail.uoguelph.ca>
2018-11-27 12:25:45 -0500

    Update edit.php

--- create/edit.php
+++ create/edit.php
@@ -7,33 +7,42 @@ $title = "Create";
 include $dir."inc/header.php";
 include $dir."inc/menu.php";
 $day = $_GET['day'];
+    
+    
+if ($_POST['finish']){
+    
+    echo "Text info";
+    
+    
+}    
+
 ?>
 
 <body>
     <div class="textbox">
         <h1>Create New Slide</h1><br>
-            <form method="get" action="">
+            <form method="post" action="">
                 <div class="row">
                 <div class="col-md-1">
                     <img src="../images/2x2.jpg" class="gridImg"><br>
-                    <input class="gridRadio" checked type="radio">
+                    <input class="gridRadio" checked value="2x2" name="gridLayout" type="radio">
                 </div>
                 <div class="col-md-1">
                     <img src="../images/3x3.jpg" class="gridImg"><br>
-                    <input class="gridRadio" disabled type="radio">
+                    <input class="gridRadio" disabled value="3x3" name="gridLayout" type="radio">
                 </div>
                 <div class="col-md-1">
                     <img src="../images/3x1.jpg" class="gridImg"><br>
-                    <input class="gridRadio" disabled type="radio">
+                    <input class="gridRadio" disabled value="3x1" name="gridLayout" type="radio">
                 </div>
                 <div class="col-md-3">
                     <img src="../images/varied.jpg" class="gridImg"><br>
-                    <input class="gridRadio" disabled type="radio">
+                    <input class="gridRadio" value="mix" name="gridLayout" disabled type="radio">
                 </div>
                   
                     
                 <div class="col-md-2">
-                <p>Transition: <select>
+                <p>Transition: <select required name="transition">
                 <option value="Fade">Fade</option>
                 <option value="Wipe">Wipe</option>
                 <option value="Dissolve">Dissolve</option>
@@ -42,7 +51,7 @@ $day = $_GET['day'];
                 </div>
                     
                 <div class="col-md-2">
-                <p>Color: <input type="color"></p>
+                <p>Color: <input required name="background" type="color"></p>
                 </div>
                 </div>
                 
@@ -51,38 +60,38 @@ $day = $_GET['day'];
                 <table border="1" cellpadding="50">
                 <tr>
                 <td>
-                    <select>
+                    <select required name="1">
                       <option value="Weather">Weather</option>
                       <option value="Time">Time</option>
                       <option value="Text">Text</option>
                     </select><br> 
-                    <input type="text">
+                    <input required name="content1" type="text">
                 </td>  
                 <td>
-                    <select>
+                    <select required name="2">
                       <option value="Weather">Weather</option>
                       <option value="Time">Time</option>
                       <option value="Text">Text</option>
                     </select><br>
-                    <input type="text">
+                    <input required name="content2" type="text">
                 </td>
                 </tr>
                 <tr>
                 <td>
-                    <select>
+                    <select required name="3">
                       <option value="Weather">Weather</option>
                       <option value="Time">Time</option>
                       <option value="Text">Text</option>
                     </select><br>
-                    <input type="text">
+                    <input required name="content3" type="text">
                 </td>  
                 <td>
-                    <select>
+                    <select required name="4">
                       <option value="Weather">Weather</option>
                       <option value="Time">Time</option>
                       <option value="Text">Text</option>
                     </select><br>
-                    <input type="text">
+                    <input required name="content4" type="text">
                 </td>
                 </tr>
                 </table>
@@ -91,7 +100,7 @@ $day = $_GET['day'];
                 <img src="../images/widgets.png">
                 <br><br>
                 
-                <button><a style="color: black; text-decoration:none;" href="../index.php?login=1">Finish&nbsp;<i class="material-icons">arrow_right_alt</i></a></button>
+                <button name="finish"><a style="color: black; text-decoration:none;" href="../index.php?login=1">Finish&nbsp;<i class="material-icons">arrow_right_alt</i></a></button>
             </form>
         </div>
     </body>

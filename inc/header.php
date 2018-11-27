f94afe5b0b92f9ea6e95c35cc1e7ea4d010e9103
msnow01 <msnow01@mail.uoguelph.ca>
2018-11-26 19:27:56 -0500

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -4,8 +4,8 @@
        <meta name="author" content="Group 2">
        <meta name="viewport" content="width=device-width, initial-scale=1">
        <link rel="shortcut icon" href="<?php echo $dir; ?>images/logo.png"/>
+        <link rel="stylesheet" href="<?php echo $dir; ?>css/styles.css">
        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-       <link rel="stylesheet" href="<?php echo $dir; ?>css/styles.css">
        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
        <script src="https://www.w3schools.com/lib/w3data.js"></script>
        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>

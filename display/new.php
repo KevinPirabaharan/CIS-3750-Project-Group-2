6e27f3d2206914812a1228b6b0f45333396befc1
msnow01 <msnow01@mail.uoguelph.ca>
2018-11-28 00:02:38 -0500

    Create new.php

---
+++ display/new.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Reynolds sign-in</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body style="background-color: moccasin; font-size: 100%; color: saddlebrown;">
+    <table border="1" style="width: 100%; ">
+        <tr>
+            <td colspan="2">
+                <table border="0" style="width:100%">
+                    <tr>
+                        <td style="width: 20%; align-content: center; "><img src="image/Gryphon.jpg" width="40%" style="padding-left: 10%;padding-right: 10%"></td>
+                        <td colspan="1" style="text-align: center; "><div style="font-size: 400%">University of Guelph</div><div style="font-size: 200%">Reynolds Building</div><div style="font-size: 150%">School of Computer Science </div></td>
+                        <td style="width: 15%; align-content: center; "><img src="image/uofg_cornerstone_red.png" width="40%" style="padding-left: 10%; padding-right: 5%;"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="3"> <hr style=" border-color: saddlebrown"> </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+
+        <tr>
+            <td style="width:50%; height:350px; align-content:center; text-align:center; font-size:150%">
+                <b>Today's Events</b><div style="font-size :50%;">&emsp;</div>
+
+                <table style="font-size :90%;  border:1px solid SaddleBrown; width:100%;border-collapse: collapse;">
+                    <tr>
+                        <th>Time</th>
+                        <th>Hall</th>
+                        <th>Event</th>
+                    </tr>
+                    <tr>
+                        <td>08:00</td>
+                        <td>203</td>
+                        <td>CIS 3750 Software Design - Dana Rea</td>
+                    </tr>
+                    <tr>
+                        <td>09:00</td>
+                        <td>427</td>
+                        <td>CIS 4750 Software Testing - Dana Rea</td>
+                    </tr>
+                    <tr>
+                        <td>10:00</td>
+                        <td>127</td>
+                        <td>CIS 2120 Assembly Language - Dana Rea</td>
+                    </tr>
+                    <tr>
+                        <td>11:00</td>
+                        <td>327</td>
+                        <td>CIS 2750 Java Programming - Dana Rea</td>
+                    </tr>
+                    <tr>
+                        <td>12:00</td>
+                        <td>203</td>
+                        <td>CIS 3750 Software Design - Dana Rea</td>
+                    </tr>
+                    <tr>
+                        <td>01:00</td>
+                        <td>427</td>
+                        <td>CIS 4750 Software Testing - Dana Rea</td>
+                    </tr>
+                    <tr>
+                        <td>02:00</td>
+                        <td>127</td>
+                        <td>CIS 2120 Assembly Language - Dana Rea</td>
+                    </tr>
+                    <tr>
+                        <td>03:00</td>
+                        <td>327</td>
+                        <td>CIS 2750 Java Programming - Dana Rea</td>
+                    </tr>
+                </table>
+            </td>
+            <td style="width:50%;height:350px; ">
+                <iframe src="https://www.youtube.com/embed/4_PfSD7uw0M?autoplay=1&controls=0&loop=1&mute=1&playlist=4_PfSD7uw0M&amp;theme=light&amp;autohide=1&amp;showinfo=0" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="1" style="width:50%; padding: .2%; align-content: center; height:350px;">
+                <a class="twitter-timeline" data-height="100%" href="https://twitter.com/uofg?ref_src=twsrc%5Etfw">Tweets by uofg</a>
+                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </td>
+            <td colspan="1" style="font-size :200%; text-align :center; vertical-align: top; padding: .5%">
+                Co-op / Job Oppurtunities<br><div style="font-size :25%;">&emsp;</div>
+
+                <table style="font-size :65%; border:1px solid SaddleBrown; width:100%;border-collapse: collapse;">
+                    <tr>
+                        <th>Apply before</th>
+                        <th>Position</th>
+                        <th>Company Details</th>
+                    </tr>
+                    <tr>
+                        <td>15 Dec</td>
+                        <td>Project Manager</td>
+                        <td>Picturisque Designs</td>
+                    </tr>
+                    <tr>
+                        <td>20 Dec</td>
+                        <td>Project Architect</td>
+                        <td>Picturisque Designs</td>
+                    </tr>
+                    <tr>
+                        <td>01 Jan</td>
+                        <td>Database Manager</td>
+                        <td>Picturisque Designs</td>
+                    </tr>
+                    <tr>
+                        <td>02 Jan</td>
+                        <td>Team Lead</td>
+                        <td>Picturisque Designs</td>
+                    </tr>
+                    <tr>
+                        <td>10 Jan</td>
+                        <td>Backend Coder</td>
+                        <td>Picturisque Designs</td>
+                    </tr>
+                    <tr>
+                        <td>13 Jan</td>
+                        <td>Layout Designer</td>
+                        <td>Picturisque Designs</td>
+                    </tr>
+                    <tr>
+                        <td>15 Jan</td>
+                        <td>Front end Developer</td>
+                        <td>Picturisque Designs</td>
+                    </tr>
+                    <tr>
+                        <td>25 Jan</td>
+                        <td>Web developer</td>
+                        <td>Picturisque Designs</td>
+                    </tr>
+                    <tr>
+                        <td>10 Jan</td>
+                        <td>Backend Coder</td>
+                        <td>Picturisque Designs</td>
+                    </tr>
+                    <tr>
+                        <td>13 Jan</td>
+                        <td>Project Manager</td>
+                        <td>Data Com</td>
+                    </tr>
+                    <tr>
+                        <td>15 Jan</td>
+                        <td>Java Developer</td>
+                        <td>Data Com</td>
+                    </tr>
+                    <tr>
+                        <td>25 Jan</td>
+                        <td>Web developer</td>
+                        <td>Data Com</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="align-content: center; text-align: center; vertical-align: 10%"> <img src="image/logo.png" width="2%"> Picturesque Designs </td>
+        </tr>
+    </table>
+
+
+    <br><br><br>
+    &emsp;&emsp;<a href="sign-in.html">Modify Signage</a><b>&emsp;&emsp;Members - Instructor: </b>
+
+    Dana Rea.&emsp;&emsp;&emsp;&emsp;
+
+    <b>Teaching Assistant:</b> Fanny Susilo, Nicolas Durish, Tamizhselvan Venkatesan.&emsp;&emsp;&emsp;&emsp;
+
+    <b>Project Lead:</b> Ahmed Mousa.&emsp;&emsp;&emsp;&emsp;
+    <b>Developers:</b> Jeremie Fraeys de Veubeke, Justin Wegner, Kevin Pirabaharan, Manoj Naick, Miriam Snow
+
+    <br><br><br><br><br>
+</body>
+</html>

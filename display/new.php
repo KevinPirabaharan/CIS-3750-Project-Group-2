d2fb26caea5fba1b5328e91c8216ca7bedcc8dec
msnow01 <msnow01@mail.uoguelph.ca>
2018-11-28 00:46:28 -0500

    Update new.php

--- display/new.php
+++ display/new.php
@@ -90,7 +90,7 @@ window.onload=changeImg;
         </tr>
     
         <tr>
-            <td colspan="2" style="font-size :200%; text-align :center; padding-top: .5%;vertical-align: top" width="30%">
+            <td colspan="2" style="font-size :200%; text-align :center; padding-top: .5%;vertical-align: top" width="25%">
                 Faculty<div style="font-size :25%;">&emsp;<br/><br/><br/><br/></div>
                 <table style="width:100%;">
                     <tr>
@@ -105,7 +105,7 @@ window.onload=changeImg;
             </td>
             <!--slide show ends-->
 
-            <td colspan="2" style="font-size :200%; text-align :center; vertical-align: top; padding: .5%; width: 27%">
+            <td colspan="2" style="font-size :200%; text-align :center; vertical-align: top; padding: .5%; width: 30%">
                 Today's Events<div style="font-size :25%;">&emsp;<br/><br/></div><br/>
                     
                 <table style="font-size :65%;  border:1px solid SaddleBrown; width:100%;border-collapse: collapse; ">
@@ -157,7 +157,7 @@ window.onload=changeImg;
                 </table>
             </td>
             
-            <td colspan="2" style=" height:300px; align-content: center">
+            <td colspan="2" style=" height:300px; align-content: center" width="25%">
                   <!-- weather widget start --><div id="m-booked-weather-bl250-45150"> <div class="booked-wzs-250-175 weather-customize" style="background-color:#ffd4b5;width:250px;" id="width1"> <div class="booked-wzs-250-175_in"> <div class="booked-wzs-250-175-data"> <div class="booked-wzs-250-175-left-img wrz-01"> <a target="_blank" href="https://www.booked.net/"> <img src="//s.bookcdn.com/images/letter/logo.gif" alt="booked net" /> </a> </div> <div class="booked-wzs-250-175-right"> <div class="booked-wzs-day-deck"> <div class="booked-wzs-day-val"> <div class="booked-wzs-day-number"><span class="plus">+</span>14</div> <div class="booked-wzs-day-dergee"> <div class="booked-wzs-day-dergee-val">&deg;</div> <div class="booked-wzs-day-dergee-name">C</div> </div> </div> <div class="booked-wzs-day"> <div class="booked-wzs-day-d">H: <span class="plus">+</span>17&deg;</div> <div class="booked-wzs-day-n">L: <span class="plus">+</span>14&deg;</div> </div> </div> <div class="booked-wzs-250-175-info"> <div class="booked-wzs-250-175-city">Guelph </div> <div class="booked-wzs-250-175-date">Monday, 08 October</div> <div class="booked-wzs-left"> <span class="booked-wzs-bottom-l">See 7-Day Forecast</span> </div> </div> </div> </div> <a target="_blank" href="https://www.booked.net/weather/guelph-30546"> <table cellpadding="0" cellspacing="0" class="booked-wzs-table-250"> <tr> <td>Tue</td> <td>Wed</td> <td>Thu</td> <td>Fri</td> <td>Sat</td> <td>Sun</td> </tr> <tr> <td class="week-day-ico"><div class="wrz-sml wrzs-03"></div></td> <td class="week-day-ico"><div class="wrz-sml wrzs-18"></div></td> <td class="week-day-ico"><div class="wrz-sml wrzs-18"></div></td> <td class="week-day-ico"><div class="wrz-sml wrzs-18"></div></td> <td class="week-day-ico"><div class="wrz-sml wrzs-18"></div></td> <td class="week-day-ico"><div class="wrz-sml wrzs-18"></div></td> </tr> <tr> <td class="week-day-val"><span class="plus">+</span>24&deg;</td> <td class="week-day-val"><span class="plus">+</span>23&deg;</td> <td class="week-day-val"><span class="plus">+</span>20&deg;</td> <td class="week-day-val"><span class="plus">+</span>11&deg;</td> <td class="week-day-val"><span class="plus">+</span>11&deg;</td> <td class="week-day-val"><span class="plus">+</span>13&deg;</td> </tr> <tr> <td class="week-day-val"><span class="plus">+</span>18&deg;</td> <td class="week-day-val"><span class="plus">+</span>17&deg;</td> <td class="week-day-val"><span class="plus">+</span>9&deg;</td> <td class="week-day-val"><span class="plus">+</span>8&deg;</td> <td class="week-day-val"><span class="plus">+</span>4&deg;</td> <td class="week-day-val"><span class="plus">+</span>8&deg;</td> </tr> </table> </a> </div></div> </div><script type="text/javascript"> var css_file=document.createElement("link"); css_file.setAttribute("rel","stylesheet"); css_file.setAttribute("type","text/css"); css_file.setAttribute("href",'https://s.bookcdn.com/css/w/booked-wzs-widget-275.css?v=0.0.1'); document.getElementsByTagName("head")[0].appendChild(css_file); function setWidgetData(data) { if(typeof(data) != 'undefined' && data.results.length > 0) { for(var i = 0; i < data.results.length; ++i) { var objMainBlock = document.getElementById('m-booked-weather-bl250-45150'); if(objMainBlock !== null) { var copyBlock = document.getElementById('m-bookew-weather-copy-'+data.results[i].widget_type); objMainBlock.innerHTML = data.results[i].html_code; if(copyBlock !== null) objMainBlock.appendChild(copyBlock); } } } else { alert('data=undefined||data.results is empty'); } } </script> <script type="text/javascript" charset="UTF-8" src="https://widgets.booked.net/weather/info?action=get_weather_info&ver=6&cityID=30546&type=3&scode=2&ltid=3457&domid=w209&anc_id=88790&cmetric=1&wlangID=1&color=0a8dff&wwidth=250&header_color=ffffff&text_color=333333&link_color=08488D&border_form=1&footer_color=ffffff&footer_text_color=333333&transparent=0"></script><!-- weather widget end -->
             
             </td>
@@ -165,10 +165,10 @@ window.onload=changeImg;
         
         
         <tr style="height:80%;">
-            <td colspan="2" style="width:33%; padding: .2%; align-content: center">
+            <td colspan="2" style="width:25%; padding: .2%; align-content: center">
                     <a class="twitter-timeline" data-height="100%" href="https://twitter.com/uofg?ref_src=twsrc%5Etfw">Tweets by uofg</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
             </td>
-            <td colspan="2" style="font-size :200%; text-align :center; vertical-align: top; padding: .5%">
+            <td colspan="2" style="font-size :200%; text-align :center; vertical-align: top; padding: .5%" width="30%">
 
                     Co-op / Job Oppurtunities<br><div style="font-size :25%;">&emsp;</div>
                     
@@ -241,7 +241,7 @@ window.onload=changeImg;
                     </table>
 
             </td>
-            <td colspan="2" style="width:34%; padding: .2%">
+            <td colspan="2" style="width:25%; padding: .2%">
                     <iframe src="https://www.youtube.com/embed/4_PfSD7uw0M?autoplay=1&controls=0&loop=1&mute=1&playlist=4_PfSD7uw0M&amp;theme=light&amp;autohide=1&amp;showinfo=0" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
             </td>
             
